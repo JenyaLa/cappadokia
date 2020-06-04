d1d0be093db2acb9ff96c170ea39d53d259461e9
Jenya Ladutsko <ladutsko1991@gmail.com>
2020-06-04 15:30:59 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,18 @@
+$(function(){
+    $('.fairy-tail__slider').slick({
+        prevArrow: '<button type="button" class="slick-btn slick-prev"><img src="images/left.svg" alt=""></button>',
+        nextArrow: '<button type="button" class="slick-btn slick-next"><img src="images/right.svg" alt=""></button>',
+        autoplay: true,
+        fade: true
+    });
+});
+
+$(function(){
+    $('.our-trip__slider').slick({
+        prevArrow: '<button type="button" class="slick-btn slick-prev"><img src="images/left.svg" alt=""></button>',
+        nextArrow: '<button type="button" class="slick-btn slick-next"><img src="images/right.svg" alt=""></button>',
+        autoplay: true,
+        fade: true
+    });
+});
+

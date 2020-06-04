2e739a05f97fc5caf640ed29e2090951e3f937c4
Jenya Ladutsko <ladutsko1991@gmail.com>
2020-06-04 21:31:20 +0300

    second commit

--- js/main.js
+++ js/main.js
@@ -3,7 +3,15 @@ $(function(){
         prevArrow: '<button type="button" class="slick-btn slick-prev"><img src="images/left.svg" alt=""></button>',
         nextArrow: '<button type="button" class="slick-btn slick-next"><img src="images/right.svg" alt=""></button>',
         autoplay: true,
-        fade: true
+        fade: true,
+        responsive: [
+            {
+            breakpoint: 601,
+            settings: {
+                arrows: false
+                }
+            }
+        ]
     });
 });
 
@@ -12,7 +20,15 @@ $(function(){
         prevArrow: '<button type="button" class="slick-btn slick-prev"><img src="images/left.svg" alt=""></button>',
         nextArrow: '<button type="button" class="slick-btn slick-next"><img src="images/right.svg" alt=""></button>',
         autoplay: true,
-        fade: true
+        fade: true,
+        responsive: [
+        {
+        breakpoint: 601,
+        settings: {
+            arrows: false
+                }
+            }
+        ]    
     });
 });
 

557efcba1600e17ceb63096636cfccfba502da88
Jenya Ladutsko <ladutsko1991@gmail.com>
2020-06-05 15:20:39 +0300

    third commit

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 $(function(){
-    $('.fairy-tail__slider').slick({
+    $('.fairy-tail__slider, .our-trip__slider').slick({
         prevArrow: '<button type="button" class="slick-btn slick-prev"><img src="images/left.svg" alt=""></button>',
         nextArrow: '<button type="button" class="slick-btn slick-next"><img src="images/right.svg" alt=""></button>',
         autoplay: true,
@@ -10,25 +10,25 @@ $(function(){
             settings: {
                 arrows: false
                 }
-            }
+            },
         ]
     });
 });
 
-$(function(){
-    $('.our-trip__slider').slick({
-        prevArrow: '<button type="button" class="slick-btn slick-prev"><img src="images/left.svg" alt=""></button>',
-        nextArrow: '<button type="button" class="slick-btn slick-next"><img src="images/right.svg" alt=""></button>',
-        autoplay: true,
-        fade: true,
-        responsive: [
-        {
-        breakpoint: 601,
-        settings: {
-            arrows: false
-                }
-            }
-        ]    
-    });
-});
+// $(function(){
+//     $('.our-trip__slider').slick({
+//         prevArrow: '<button type="button" class="slick-btn slick-prev"><img src="images/left.svg" alt=""></button>',
+//         nextArrow: '<button type="button" class="slick-btn slick-next"><img src="images/right.svg" alt=""></button>',
+//         autoplay: true,
+//         fade: true,
+//         responsive: [
+//         {
+//         breakpoint: 601,
+//         settings: {
+//             arrows: false
+//                 }
+//             }
+//         ]    
+//     });
+// });
 
